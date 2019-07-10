c92ae080798944f48a9614088c7e4720d2d8c184
junpei <junpei@hacarus.com>
2019-07-11 00:51:42 +0900

    add parameters

--- total_variation_3d.py
+++ total_variation_3d.py
@@ -10,6 +10,9 @@ class TotalVariation3D:
     """
     def __init__(self,
             lambd: float = 1.0,
+            param_d: float = 1.0,
+            param_h: float = 1.0,
+            param_w: float = 1.0,
             max_iter: int = 1000,
             coef: np.ndarray = np.array([1, -1]),
             tol: float = 1e-3,
@@ -33,6 +36,9 @@ class TotalVariation3D:
             If True, return the value of the objective function of each iteration.
         """
         self.lambd = lambd
+        self.param_d = param_d
+        self.param_h = param_h
+        self.param_w = param_w
         self.max_iter = max_iter
         self.coef = coef
         self.tol = tol
@@ -53,18 +59,22 @@ class TotalVariation3D:
         d, h, w = shape
         hw = h * w
         dhw = d * hw
-        index1 = dhw - self.length
-        index2 = w * (h - self.length)
-        ret = np.empty(3 * dhw - self.length * (hw + w * d + 1))
+        index1 = (dhw - self.length) * (self.param_w > 0)
+        index2 = w * (h - self.length) * (self.param_h > 0)
+        shape = index1 + d * index2 + (hw * (d - self.length)) * (self.param_d > 0)
+        ret = np.empty(shape)
 
-        ret[: index1] = self._tv_one(u, step=1)
-        for i in range(self.length):
-            ret[: index1][w - self.length + i::w] = 0
+        if self.param_w > 0:
+            ret[: index1] = self._tv_one(u, step=1) * self.param_w
+            for i in range(self.length):
+                ret[: index1][w - self.length + i::w] = 0
 
-        for i in range(d):
-            ret[index1 + i * index2: index1 + (i + 1) * index2] = self._tv_one(u[i * hw : (i + 1) * hw], step=w)
+        if self.param_h > 0:
+            for i in range(d):
+                ret[index1 + i * index2: index1 + (i + 1) * index2] = self._tv_one(u[i * hw : (i + 1) * hw], step=w) * self.param_h
 
-        ret[index1 + d * index2 :] = self._tv_one(u, step=hw)
+        if self.param_d > 0:
+            ret[index1 + d * index2 :] = self._tv_one(u, step=hw) * self.param_d
         return ret
 
     def _transposed_tv_one(self, v: np.ndarray, step: int) -> np.ndarray:
@@ -78,39 +88,51 @@ class TotalVariation3D:
         d, h, w = shape
         hw = h * w
         dhw = d * hw
-        index1 = dhw - self.length
-        index2 = w * (h - self.length)
+        index1 = (dhw - self.length) * (self.param_w > 0)
+        index2 = w * (h - self.length) * (self.param_h > 0)
+        ret = np.zeros(dhw)
 
-        ret = self._transposed_tv_one(v[: index1], step=1)
+        if self.param_w > 0:
+            ret += self._transposed_tv_one(v[: index1], step=1) * self.param_w
 
-        for i in range(d):
-            ret[i * hw: (i + 1) * hw] += self._transposed_tv_one(v[index1 + i * index2 : index1 + (i + 1) * index2], step=w)
+        if self.param_h > 0:
+            for i in range(d):
+                ret[i * hw: (i + 1) * hw] += self._transposed_tv_one(v[index1 + i * index2 : index1 + (i + 1) * index2], step=w) * self.param_h
 
-        ret += self._transposed_tv_one(v[index1 + d * index2 :], step=hw)
+        if self.param_d > 0:
+            ret += self._transposed_tv_one(v[index1 + d * index2 :], step=hw) * self.param_d
         return ret
 
     def _step_size(self, shape: Tuple[int, int, int]) -> Tuple[np.ndarray, np.ndarray]:
         d, h, w = shape
         hw = h * w
         dhw = d * hw
-        index1 = w * (h - self.length)
-        index2 = h * w * (d - self.length)
+        index2 = w * (h - self.length) * (self.param_h > 0)
+        index3 = h * w * (d - self.length)
+        shape = (dhw - self.length) * (self.param_w > 0) + d * index2 + (hw * (d - self.length)) * (self.param_d > 0)
         abs_coef = np.abs(self.coef)
         abs_sum = np.sum(abs_coef)
 
-        tau = np.full(dhw, self.lambd + abs_sum)
-        for i in range(self.length):
-            tau[i] -= np.sum(abs_coef[i + 1 :])
-            tau[-(i + 1)] -= np.sum(abs_coef[: -(i + 1)])
-        tile = np.zeros(hw)
-        for i, c in enumerate(abs_coef):
-            tile[w * i : w * i + index1] += c
-        tau += np.tile(tile, d)
-        for i, c in enumerate(abs_coef):
-            tau[hw * i : hw * i + index2] += c
+        tau = np.full(dhw, self.lambd)
+
+        if self.param_w > 0:
+            tau += abs_sum
+            for i in range(self.length):
+                tau[i] -= np.sum(abs_coef[i + 1 :])
+                tau[-(i + 1)] -= np.sum(abs_coef[: -(i + 1)])
+
+        if self.param_h > 0:
+            tile = np.zeros(hw)
+            for i, c in enumerate(abs_coef):
+                tile[w * i : w * i + index2] += c
+            tau += np.tile(tile, d)
+
+        if self.param_d > 0:
+            for i, c in enumerate(abs_coef):
+                tau[hw * i : hw * i + index3] += c
         tau = 1. / tau
 
-        sigma = np.zeros(4 * d * hw - self.length * (hw + w * d + 1))
+        sigma = np.zeros(dhw + shape)
         sigma[:-dhw] += abs_sum
         sigma[-dhw:] += self.lambd
         sigma = 1. / sigma
@@ -133,12 +155,15 @@ class TotalVariation3D:
         d, h, w = X.shape[:3]
         hw = h * w
         dhw = hw * d
+        shape = (dhw - self.length) * (self.param_w > 0) \
+            + d * w * (h - self.length) * (self.param_h > 0) \
+            + (hw * (d - self.length)) * (self.param_d > 0)
         tau, sigma = self._step_size((d, h, w))
 
         # initialize
         x = X.flatten()
         res = np.copy(x)
-        dual = np.zeros(4 * dhw - self.length * (hw + w * d + 1))
+        dual = np.zeros(dhw + shape)
         dual[:-dhw] = np.clip(sigma[:-dhw] * self._tv(res, (d, h, w)), -1, 1)
 
         # objective function
@@ -160,6 +185,6 @@ class TotalVariation3D:
             res = u
             if self.extended_output:
                 obj.append(np.sum(np.abs(self._tv(res, (d, h, w)))) + self.lambd * np.sum(np.abs(res - x)))
-            if np.linalg.norm(diff) / (np.linalg.norm(res) + self.eps) < self.tol:
-                break
+            # if np.linalg.norm(diff) / (np.linalg.norm(res) + self.eps) < self.tol:
+            #     break
         return res.reshape((d, h, w)), obj
